6db12c531307f59f929f16becd909717edc494e3
Hanker <anhan0804@dev-jitsu.com>
2024-11-04 10:45:54 +0900

    Initial commit

---
+++ src/main/java/com/hankersf/HankerSfApplication.java
@@ -0,0 +1,13 @@
+package com.hankersf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HankerSfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HankerSfApplication.class, args);
+	}
+
+}
